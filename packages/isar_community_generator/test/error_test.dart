be9f0c27eab21ab51277f74aea7c897172147d4c
Callum Wellwood-Kane <business@callumwk.com>
2025-09-02 15:36:40 +1000

    fixed error_test file imports

--- packages/isar_community_generator/test/error_test.dart
+++ packages/isar_community_generator/test/error_test.dart
@@ -6,7 +6,7 @@ import 'package:isar_community_generator/isar_generator.dart';
 import 'package:logging/logging.dart';
 import 'package:test/test.dart';
 
-void main() {
+void main() async {
   group('Error case', () {
     for (final file in Directory('test/errors').listSync(recursive: true)) {
       if (file is! File || !file.path.endsWith('.dart')) continue;
@@ -18,73 +18,15 @@ void main() {
 
         var error = '';
         try {
-          final assets = {
-            'a|${file.path}': content.join('\n'),
-          };
-
-          const isarPath = '../isar_community/lib';
-          assets.addAll({
-            'isar_community|lib/isar.dart':
-                await File('$isarPath/isar.dart').readAsString(),
-            'isar_community|lib/src/annotations/collection.dart':
-                await File(
-                  '$isarPath/src/annotations/collection.dart',
-                ).readAsString(),
-            'isar_community|lib/src/annotations/embedded.dart':
-                await File(
-                  '$isarPath/src/annotations/embedded.dart',
-                ).readAsString(),
-            'isar_community|lib/src/annotations/index.dart':
-                await File(
-                  '$isarPath/src/annotations/index.dart',
-                ).readAsString(),
-            'isar_community|lib/src/annotations/name.dart':
-                await File(
-                  '$isarPath/src/annotations/name.dart',
-                ).readAsString(),
-            'isar_community|lib/src/annotations/backlink.dart':
-                await File(
-                  '$isarPath/src/annotations/backlink.dart',
-                ).readAsString(),
-            'isar_community|lib/src/annotations/ignore.dart':
-                await File(
-                  '$isarPath/src/annotations/ignore.dart',
-                ).readAsString(),
-            'isar_community|lib/src/annotations/enumerated.dart':
-                await File(
-                  '$isarPath/src/annotations/enumerated.dart',
-                ).readAsString(),
-            'isar_community|lib/src/annotations/type.dart':
-                await File(
-                  '$isarPath/src/annotations/type.dart',
-                ).readAsString(),
-            'isar_community|lib/src/isar.dart':
-                await File('$isarPath/src/isar.dart').readAsString(),
-            'isar_community|lib/src/isar_link.dart':
-                await File('$isarPath/src/isar_link.dart').readAsString(),
-            'isar_community|lib/src/schema/collection_schema.dart':
-                await File(
-                  '$isarPath/src/schema/collection_schema.dart',
-                ).readAsString(),
-            'isar_community|lib/src/schema/property_schema.dart':
-                await File(
-                  '$isarPath/src/schema/property_schema.dart',
-                ).readAsString(),
-            'isar_community|lib/src/schema/index_schema.dart':
-                await File(
-                  '$isarPath/src/schema/index_schema.dart',
-                ).readAsString(),
-            'isar_community|lib/src/schema/link_schema.dart':
-                await File(
-                  '$isarPath/src/schema/link_schema.dart',
-                ).readAsString(),
-            'isar_community|lib/src/schema/schema.dart':
-                await File('$isarPath/src/schema/schema.dart').readAsString(),
-          });
+          final readerWriter = TestReaderWriter(
+            rootPackage: 'isar_community_generator',
+          );
+          await readerWriter.testing.loadIsolateSources();
 
           await testBuilder(
             getIsarGenerator(BuilderOptions.empty),
-            assets,
+            {'a|${file.path}': content.join('\n')},
+            readerWriter: readerWriter,
             onLog: (record) {
               if (record.level >= Level.SEVERE) {
                 error = record.message;
